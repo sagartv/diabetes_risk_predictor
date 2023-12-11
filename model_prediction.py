ed2e315bc21ebcf7877bcb68d6a723fe99b784d1
sagartv <31515867+sagartv@users.noreply.github.com>
2023-12-11 08:24:30 +0000

    Added Progress Bar to Probability Display

--- model_prediction.py
+++ model_prediction.py
@@ -24,6 +24,7 @@ def predict_diabetes(instance,model):
   instance = preprocess_input(instance)
   prediction = model.predict(instance)
   probability = model.predict_proba(instance)[0][1] * 100.0
+  probability = round(probability)
   return prediction, probability
   
     
